284592f573c5acb217c55e654385b06fa929cac9
Fangels <allan.brongniart@coding-academy.fr>
2017-11-29 18:43:19 +0100

    core

--- Config/core.php
+++ Config/core.php
@@ -1,8 +1,28 @@
 <?php
+    $basePath = __DIR__;
  class Framework
  {
      public static function run()
      {
          echo "run()";
+     }
+
+     private static function init()
+     {
+         // Load core classes
+         require $basePath."/Controllers/ArticlesController.php";
+         require $basePath."/Controllers/UsersController.php";
+         require $basePath."/Config/Db.php";
+         require $basePath."/Models/Article.php";
+         require $basePath."/Src/log.php";
+         require $basePath."/Src/session.php";
+         require $basePath."/Src/router.php";
+
+         // Load configuration file
+         include $basePath."/Config/config.php";
+
+
+         // Start session
+         session_start();
      }
  }
