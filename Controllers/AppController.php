283491d2e1ab2baa33c93d8fbc8441e6e28a57f9
noriane.rozales <noriane.rozales@coding-academy.fr>
2017-11-30 16:01:07 +0100

    create appController

--- Controllers/AppController.php
+++ Controllers/AppController.php
@@ -2,10 +2,30 @@
 
 abstract class AppController
 {
+	protected $_model;
+	protected $_file;
+	protected $_params = [];
 
-  function __construct()
-  {
-    # code...
-  }
+	private function __construct()
+	{
+		# code...
+	}
+
+	public function loadModel($model)
+	{
+		$this->_model = new $model();
+	}
+
+	public function beforeRender();
+
+	public function render($file=null)
+	{
+		$this->_file = new View($file);
+	}
+
+	protected function redirect($url)
+	{
+		
+	}
 }
- ?>
+?>
