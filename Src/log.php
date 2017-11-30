0d67415cfc9f7d9b0c826d5400960f9fd71ff48f
Fangels <allan.brongniart@coding-academy.fr>
2017-11-30 16:02:07 +0100

    Db //  log

--- Src/log.php
+++ Src/log.php
@@ -3,49 +3,50 @@ include_once($basePath."/Config/Db.php");
 
 class Log
 {
-  private $_id;
-  private $_bdd;
+    private $_id;
+    private $_bdd;
 
-  public function __construct($id)
-  {
-    $this->_id = $id;
-    $this->connect();
-  }
+    public function __construct($id)
+    {
+        $this->_id = $id;
+        $this->connect();
+    }
 
-  private function connect()
-  {
-    $this->_bdd = new Db();
-  }
+    private function connect()
+    {
+        $this->_bdd = Db::getInstance();
+    }
 
-  public function is_group()
-  {
-    $req = "SELECT admin from users WHERE id = '$this->_id'";
-    $donne = $this->_bdd->SQLquery($req);
+    public function is_group()
+    {
+        $req = "SELECT admin from users WHERE id = '$this->_id'";
+        $this->_bdd->setQuery($req);
+        $donne = $this->_bdd->SQLquery();
         if ($donne["group"] == "admin") {
-      return "admin";
-    }else {if ($donne["group"] == "writer") {
-      return "writer";
+            return "admin";
+        } else {
+            if ($donne["group"] == "writer") {
+                return "writer";
+            }
+            return "user";
+        }
     }
-      return "user";
-    }
-  }
 
-  public function this_name()
-  {
-    $req = "SELECT username from users WHERE id = '$this->_id'";
-    $donne = $this->_bdd->SQLquery($req);
+    public function this_name()
+    {
+        $req = "SELECT username from users WHERE id = '$this->_id'";
+        $this->_bdd->setQuery($req);
+        $donne = $this->_bdd->SQLquery();
         return $donne["username"];
-  }
+    }
 
-  public function __sleep()
-  {
-    return array('_id');
-  }
+    public function __sleep()
+    {
+        return array('_id');
+    }
 
-  public function __wakeup()
-  {
-    $this->connect();
-  }
+    public function __wakeup()
+    {
+        $this->connect();
+    }
 }
-
- ?>
