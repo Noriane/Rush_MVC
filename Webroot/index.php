fb832df863850311a7fe7bffecd920339c6e1a8b
noriane.rozales <noriane.rozales@coding-academy.fr>
2017-11-30 17:19:28 +0100

    create first twig template

--- Webroot/index.php
+++ Webroot/index.php
@@ -2,5 +2,11 @@
     $basePath = __DIR__."/../";
 
     require $basePath.'Config/core.php';
+    require $basePath.'Views/Includes/configTwig.html';
 
     Framework::run();
+
+    $template = $twig->loadTemplate('index.twig');
+    	echo $template->render(array(
+		'moteur_name' => 'Twig'
+    )); 
