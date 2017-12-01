1930dfb7504aac3a4eccf5a460ac5564271ced9c
noriane.rozales <noriane.rozales@coding-academy.fr>
2017-12-01 09:35:26 +0100

    moteur working with semantic

--- Controllers/View.php
+++ Controllers/View.php
@@ -3,11 +3,16 @@ class View
 {
 	public function __construct($file, $params)
 	{
+		$this->_file = $file;
+		$this->_params = $params;
 
 	}
 	public function renderView()
 	{
 		//global $twig;
 		//Twig
+		//$template = $twig->loadTemplate($file);
+		$template = $twig->loadTemplate('index.twig');
+    	echo $template->render($this->$_params);
 	}
 }
