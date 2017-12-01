8d85693da9b95bdbfa3049fdbccff6de4c85dc5c
noriane.rozales <noriane.rozales@coding-academy.fr>
2017-12-01 15:11:31 +0100

    udpate maria_db mysql

---
+++ clean_maria.sh
@@ -0,0 +1,6 @@
+sudo rm /usr/local/mysql
+sudo rm -rf /usr/local/var/mysql
+sudo rm -rf /usr/local/mysql*
+sudo rm ~/Library/LaunchAgents/homebrew.mxcl.mysql.plist
+sudo rm -rf /Library/StartupItems/MySQLCOM
+sudo rm -rf /Library/PreferencePanes/My*
