36f763e8ab14d065b9b98d01299b352e8fc4e46f
Fangels <allan.brongniart@coding-academy.fr>
2017-12-01 16:08:26 +0100

    correction img sql

--- Models/Accueil.php
+++ Models/Accueil.php
@@ -4,7 +4,7 @@ class AccueilModel extends AModel
 {
     public function ten_articles()
     {
-        $sql= "SELECT articles.id, title, content, users.username, categories.name as 'cat', tag_id, CONCAT('../Webroot/Img/',url_img), articles.create_date, articles.edit_date FROM articles INNER JOIN users ON articles.author_id = users.id INNER JOIN categories ON articles.categories_id = categories.id ORDER BY articles.id DESC LIMIT 10";
+        $sql= "SELECT articles.id, title, content, users.username, categories.name as 'cat', tag_id, CONCAT('/Webroot/Img/',url_img), articles.create_date, articles.edit_date FROM articles INNER JOIN users ON articles.author_id = users.id INNER JOIN categories ON articles.categories_id = categories.id ORDER BY articles.id DESC LIMIT 10";
 
         $this->_connect->setQuery($sql);
         return $this->_connect->SQLquery(false);
