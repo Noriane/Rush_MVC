1c0f6910501dfccfd98b2411fd8479640c142827
Fangels <allan.brongniart@coding-academy.fr>
2017-12-01 18:20:11 +0100

    model admin

--- Models/Admin.php
+++ Models/Admin.php
@@ -10,5 +10,35 @@ class AccueilModel extends AModel
         return $this->_connect->SQLquery(false);
     }
 
+    public function add_user($user)
+    {
+        $name = $user['name'];
+        $pass = $user['password'];
+        $email = $user['email'];
+        $group = $user['group'];
+        $sql = "INSERT INTO users VALUES (null,'$name','$pass','$email','$group','0',NOW(),NOW()";
 
+        $this->_connect->setQuery($sql);
+        $this->_connect->SQLquery(false);
+    }
+
+    public function modif_user($user)
+    {
+        $name = $user['name'];
+        $ban = $user['ban'];
+        $email = $user['email'];
+        $group = $user['group'];
+        $sql = "UPDATE users username = '$name',email = '$email', group = '$group',ban = $ban, edit_date = NOW()";
+
+        $this->_connect->setQuery($sql);
+        $this->_connect->SQLquery(false);
+    }
+
+    public function delete_user($id)
+    {
+        $sql = " DELETE users WHERE id = '$id'";
+
+        $this->_connect->setQuery($sql);
+        $this->_connect->SQLquery(false);
+    }
 }
