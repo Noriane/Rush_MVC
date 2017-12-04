e71a2165d9b4229e5cdec88f93b4c5e9e48ba65d
Fangels <allan.brongniart@coding-academy.fr>
2017-12-04 18:47:44 +0100

    articles debug

--- Views/View.php
+++ Views/View.php
@@ -6,17 +6,17 @@ class View
     {
         include_once PATH.'/Views/Includes/configTwig.php';
         $twig = get_twig();
-        $params['session'] = $_SESSION; 
-        
+        $params['session'] = $_SESSION;
+
         $params['path'] = BASE_URL;
 
-        /*
-        echo "<pre>";
-        var_dump($params);
-        echo "<pre>";
-        */
+
+        // echo "<pre>";
+        // var_dump($params);
+        // echo "<pre>";
+        //
 
         $template = $twig->loadTemplate($file);
-        echo $template->render($params); 
+        echo $template->render($params);
     }
 }
