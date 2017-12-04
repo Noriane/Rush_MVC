11aee3be38ffc02c13ba292846d141c38cacae2f
Fangels <allan.brongniart@coding-academy.fr>
2017-12-04 17:33:09 +0100

    accueil tag fetch effectuer

--- Controllers/AccueilController.php
+++ Controllers/AccueilController.php
@@ -2,7 +2,6 @@
 
 class AccueilController extends AppController
 {
-
     protected function beforeRender()
     {
         $this->_params['data'] = $this->_model->ten_articles();
@@ -12,7 +11,11 @@ class AccueilController extends AppController
         while ($data = $this->_params['data']->fetch(PDO::FETCH_ASSOC)) {
             array_push($this->_params['articles'], $data);
             $this->_params['articles'][$i]['nb_comment']= $this->_model->nb_comment($data['id']);
-            $this->_params['articles'][$i]['tags']= $this->_model->tags($data['tag_id']);
+            $tag_temp  = $this->_model->tags($data['tag_id']);
+
+            while ($data_tag = $tag_temp->fetch(PDO::FETCH_ASSOC)) {
+                $this->_params['articles'][$i]['tags'] = $data_tag;
+            }
 
             $i++;
         }
