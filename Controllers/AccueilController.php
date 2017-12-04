1356b358f116eff0cb8671d699eada6e0aaee95b
Fangels <allan.brongniart@coding-academy.fr>
2017-12-04 17:37:31 +0100

    erreur tag fix sur accueil

--- Controllers/AccueilController.php
+++ Controllers/AccueilController.php
@@ -13,8 +13,10 @@ class AccueilController extends AppController
             $this->_params['articles'][$i]['nb_comment']= $this->_model->nb_comment($data['id']);
             $tag_temp  = $this->_model->tags($data['tag_id']);
 
-            while ($data_tag = $tag_temp->fetch(PDO::FETCH_ASSOC)) {
-                $this->_params['articles'][$i]['tags'] = $data_tag;
+            foreach ($tag_temp as $temp) {
+              while ($data_tag = $temp->fetch(PDO::FETCH_ASSOC)) {
+                  $this->_params['articles'][$i]['tags'] = $data_tag;
+              }
             }
 
             $i++;
